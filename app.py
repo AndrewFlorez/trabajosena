0b2b950d32f63d10577b8c9c28f3190b17b8a9d7
AndrewFlorez <andrewsflorez3@gmail.com>
2026-02-24 21:49:56 -0500

    prueba
    calculo de diferencia de edad entre hermanos con datos personales

---
+++ app.py
@@ -0,0 +1,12 @@
+print("ingrese su nombre: ")
+nombre=input()
+print("ingrese su apellido: ")
+apellido=input()
+print("ingrese la edad del hermano mayor: ")
+edad_mayor=int(input())
+print("ingrese la edad del hermano menor: ")
+edad_menor=int(input())
+Nombre_completo=nombre + " " + apellido 
+print("nombre completo: ",Nombre_completo)
+diferencia= edad_mayor - edad_menor
+print("la diferencia de edad entre los hermanos es: ",diferencia)
